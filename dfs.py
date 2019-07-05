c4436f1bb17b26dfca254733016b53ebbccd8c64
strea <stream.larn3@gmail.com>
2019-07-05 09:40:13 +0900

    よくわからないけど動いたからヨシ！

--- dfs.py
+++ dfs.py
@@ -4,23 +4,23 @@ from myfunc import get_next_positions
 def search_dfs(maze, start, end):
     checked = {start: None}
     solved_maze = [[], checked]
-    dfs_recursive(maze, start, end, checked)
+    if not dfs_recursive(maze, start, end, checked):
+        return []
 
-    parent_y, parent_x = end[0], end[1]
-    while not (parent_y == start[0] and parent_x == start[1]):
+    (parent_y, parent_x) = end
+    while not (parent_y, parent_x) == start:
         solved_maze[0].append((parent_y, parent_x))
-        parent_y, parent_x = solved_maze[1][parent_y, parent_x][0], solved_maze[1][parent_y, parent_x][1]
+        (parent_y, parent_x) = solved_maze[1][parent_y, parent_x]
     return solved_maze
 
 
 def dfs_recursive(maze, pos, end, checked):
     for next_pos in get_next_positions(pos, maze):
         (y, x) = next_pos
-        # ゴールに到達した場合
-        if y == end[0] and x == end[1]:
+        """ IS_GOAL?"""
+        if next_pos == end:
             checked[end[0], end[1]] = pos
             return checked
-
         # 壁でなく、かつゴールに到達していない場合
         elif maze[y][x] != "O" and (y, x) not in checked:
             checked[y, x] = pos
--- dls.py
+++ dls.py
@@ -2,12 +2,11 @@ from myfunc import get_next_positions
 
 
 def search_dls(maze, start, end):
-    count = 1
-    result = dls_bootstrap(maze, start, end, count)
+    count = 0
+    result = []
     while not result:
-        count += 1
         result = dls_bootstrap(maze, start, end, count)
-
+        count += 1
     return result
 
 
@@ -18,27 +17,28 @@ def dls_bootstrap(maze, start, end, limit):
         return []
 
     parent_y, parent_x = end[0], end[1]
-    while not (parent_y == start[0] and parent_x == start[1]):
+    while not (parent_y, parent_x) == start:
         solved_maze[0].append((parent_y, parent_x))
-        parent_y, parent_x = solved_maze[1][parent_y, parent_x][1][0], solved_maze[1][parent_y, parent_x][1][1]
+        (parent_y, parent_x) = solved_maze[1][parent_y, parent_x][1]
     return solved_maze
 
 
 def dls_recursive(maze, pos, end, checked, limit):
-    for next_pos in get_next_positions(pos, maze):
-        (y, x) = next_pos
-        cnt = checked[pos[0], pos[1]][0] + 1
-        # ゴールに到達した場合
-        if y == end[0] and x == end[1]:
-            checked[end[0], end[1]] = [cnt, pos]
-            return checked
-        elif cnt == limit:
-            return {}
-        # 壁でなく、かつゴールに到達していない場合
-        elif maze[y][x] != "O" and (y, x) not in checked:
-            checked[y, x] = [cnt, pos]
-            result = dls_recursive(maze, (y, x), end, checked, limit)
-            if result:
-                return result
-            checked.pop((y, x))
+    cnt = checked[pos[0], pos[1]][0]
+
+    if cnt < limit:
+        for next_pos in get_next_positions(pos, maze):
+            (y, x) = next_pos
+            cnt += 1
+            """ IS_GOAL?"""
+            if next_pos == end:
+                checked[end[0], end[1]] = [cnt, pos]
+                return checked
+            # 壁でなく、かつゴールに到達していない場合
+            elif maze[y][x] != "O" and (y, x) not in checked:
+                checked[y, x] = [cnt, pos]
+                result = dls_recursive(maze, (y, x), end, checked, limit)
+                if result:
+                    return result
+                checked.pop((y, x))
     return {}
